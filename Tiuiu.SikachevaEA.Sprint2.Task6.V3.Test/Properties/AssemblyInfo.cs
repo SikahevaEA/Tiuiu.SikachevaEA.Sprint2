174b7ae8ff1d21e99306d9b878cc62aca3cf76aa
Tiuiu.SikachovaEA@outlook.com <Tiuiu.SikachovaEA@outlook.com>
2023-10-15 21:22:54 +0500

    Task6

---
+++ Tiuiu.SikachevaEA.Sprint2.Task6.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tiuiu.SikachevaEA.Sprint2.Task6.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tiuiu.SikachevaEA.Sprint2.Task6.V3.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("543ee339-7f24-4f85-b03e-76121dd8b208")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tiuiu.SikachevaEA.Sprint2.Task6.V3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tiuiu.SikachevaEA.Sprint2.Task6.V3.Lib;
+//Написать программу, которая использует сокращенную форму записи оператора switch
+//вычисляет требуемое значение и возвращает результат.
+//Условие: Составить программу, которая в зависимости от порядкового номера дня недели (1, 2, …, 7)
+//выводит на экран его название (понедельник, вторник, …, воскресенье). 
+
+namespace Tiuiu.SikachevaEA.Sprint2.Task6.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Введите номер дня: ");
+            int numDay = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if ((numDay < 1) || (numDay > 7))
+            {
+                res = "Введено не правильное значение";
+            }
+            else
+            {
+                res = "Название дня: " + ds.FindDayName(numDay);
+            }
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
